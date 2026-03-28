499b0b98a2bbb5a40001977b9602f74ff35856dc
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2026-03-28 12:59:39 +0100

    Fixing some deployment error(vercel)

--- utils/generateTicketCode.ts
+++ utils/generateTicketCode.ts
@@ -1,4 +1,4 @@
-import { nanoid } from "nanoid";
+import crypto from "crypto";
 
 export const generateTicketCode = (eventTitle: string): string => {
   const prefix = eventTitle
@@ -6,9 +6,12 @@ export const generateTicketCode = (eventTitle: string): string => {
     .toUpperCase()
     .replace(/[^A-Z0-9\s]/g, "")
     .split(/\s+/)
-    .map((char) => char[0])
+    .filter(Boolean)
+    .map((word) => word[0])
     .join("")
     .slice(0, 4);
 
-  return `${prefix}-${nanoid(8).toUpperCase()}`;
+  const randomPart = crypto.randomBytes(4).toString("hex").toUpperCase();
+
+  return `${prefix || "EVT"}-${randomPart}`;
 };
