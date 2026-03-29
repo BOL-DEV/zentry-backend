887118d1c9cf40e08ff4c84c00ca7fb9b31833b7
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2026-03-29 17:50:06 +0100

    creation of dashboard users

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,9 +1,11 @@
-import { login } from "../controllers/authController";
+import { createDashboardUser, login } from "../controllers/authController";
 import { Router } from "express";
 
 const router = Router();
 
-
 router.route("/login").post(login);
+
+// Create dashboard user (organizer/staff)
+router.route("/bootstrap/users").post(createDashboardUser);
 
 export default router;
